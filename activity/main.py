eec87f330fdca6a6dfb897bc6e4dc5c7932d3cbf
thaimynguyen <thaimynguyen@gmail.com>
2022-03-23 10:45:37 -0700

    added test for IndexError

---
+++ activity/main.py
@@ -0,0 +1,20 @@
+"""
+List
+# Ask for user input
+# Enter 3 favorite food
+# If user enters less than 3 food 
+    # raise IndexError
+    # print error message "Please input exactly 3 types of food" 
+# If user enters more than 3 food
+    # print error message "Please input exactly 3 types of food"
+"""
+
+
+def top_3_food(foodlist):
+
+    print("Your top 3 favorite food is: ")
+    for i in range(3):
+        print(foodlist[i])
+
+
+# top_3_food(["a", "b"])
--- tests/test_exception.py
+++ tests/test_exception.py
@@ -2,5 +2,11 @@ import pytest
 
 from activity.main import *
 
-def test_exception():
-    assert True
+
+def test_raises_exception_with_less_than_3_food():
+    # arrange
+    foodlist = ["ramen", "kimchi"]
+
+    # act & assert
+    with pytest.raises(IndexError):
+        top_3_food(foodlist)
